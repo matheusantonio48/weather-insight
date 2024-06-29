b6be6aad903b5b31d996418d1ee8dbd258b95196
Matheus Ramalho <47503055+matheusantonio48@users.noreply.github.com>
2024-06-29 20:58:54 -0300

    ci: fixing pipeline config jest

---
+++ jest.config.ts
@@ -0,0 +1,42 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.(js|ts)(x)?', '!**/node_modules/**', '!**/designSystem/**'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [],
+  coverageReporters: ['html', 'text', 'text-summary', 'cobertura', 'lcov'],
+  coverageThreshold: {
+    global: {
+      lines: 85,
+    },
+  },
+  globals: {
+    window: {
+      location: {},
+    },
+  },
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  modulePathIgnorePatterns: ['<rootDir>/src/app/api/auth/'],
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['<rootDir>/.jest/jest.setup.ts', '<rootDir>/.jest/setEnvVars.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
+  },
+  verbose: true,
+};
+
+export default createJestConfig(config);
