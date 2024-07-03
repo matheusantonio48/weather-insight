29de95d4ea1e35a9d27e1fb93b9f8dae1fc2a0a3
Matheus Ramalho <47503055+matheusantonio48@users.noreply.github.com>
2024-07-03 19:20:11 -0300

    chore: create tests for weather home screen

---
+++ src/app/pages/weather-home/__tests__/index.test.tsx
@@ -0,0 +1,67 @@
+import { mockWeatherData } from '@/app/mocks/mockWeatherData';
+import { useWeather } from '@/context/useWeather';
+import theme from '@/theme/theme';
+import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { WeatherHome } from '../index';
+
+jest.mock('@/context/useWeather');
+
+describe('WeatherHome Component', () => {
+  const mockUseWeather = useWeather as jest.MockedFunction<typeof useWeather>;
+
+  beforeEach(() => {
+    mockUseWeather.mockReturnValue({
+      fullWeatherData: mockWeatherData,
+      unitOption: 'C',
+      getWeatherByName: jest.fn(),
+      setUnitOption: jest.fn(),
+      defaultCity: 'Campinas',
+      setDefaultCity: jest.fn(),
+      previousSearches: [],
+      addPreviousSearch: jest.fn(),
+      setFullWeatherData: jest.fn(),
+      setTodayWeather: jest.fn(),
+      todayWeather: {
+        ...mockWeatherData.current,
+        weather: mockWeatherData.current.description,
+      },
+      toggleForm: jest.fn(),
+      showForm: false,
+    });
+  });
+
+  const renderWithProviders = (component: React.ReactElement) => {
+    return render(<ThemeProvider theme={theme}>{component}</ThemeProvider>);
+  };
+
+  it('should render ResumeWeather and DetailWeather components when fullWeatherData is available', () => {
+    renderWithProviders(<WeatherHome />);
+
+    expect(screen.getByText("Campinas, Today's Highlights")).toBeInTheDocument();
+    expect(screen.getByText('Wind Status')).toBeInTheDocument();
+  });
+
+  it('should not render ResumeWeather and DetailWeather components when fullWeatherData is not available', () => {
+    mockUseWeather.mockReturnValue({
+      fullWeatherData: null,
+      unitOption: 'C',
+      getWeatherByName: jest.fn(),
+      setUnitOption: jest.fn(),
+      defaultCity: 'Campinas',
+      setDefaultCity: jest.fn(),
+      previousSearches: [],
+      addPreviousSearch: jest.fn(),
+      setFullWeatherData: jest.fn(),
+      setTodayWeather: jest.fn(),
+      todayWeather: null,
+      toggleForm: jest.fn(),
+      showForm: false,
+    });
+
+    renderWithProviders(<WeatherHome />);
+
+    expect(screen.queryByText("Campinas, Today's Highlights")).not.toBeInTheDocument();
+    expect(screen.queryByText('Wind Status')).not.toBeInTheDocument();
+  });
+});
--- src/app/pages/weather-home/index.tsx
+++ src/app/pages/weather-home/index.tsx
@@ -2,31 +2,17 @@ import Container from '@/app/components/molecules/Container';
 import DetailWeather from '@/app/components/organisms/DetailWeather';
 import ResumeWeather from '@/app/components/organisms/ResumeWeather';
 import { useWeather } from '@/context/useWeather';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 export const WeatherHome: React.FC = () => {
   const { fullWeatherData, unitOption, getWeatherByName, setUnitOption } = useWeather();
-
-  const setError = (error: boolean) => {
-    // @to-do
-    // Implementar lógica para lidar com erros
-  };
-
-  useEffect(() => {
-    console.log(fullWeatherData);
-  }, []);
 
   return (
     <div>
       <Container>
         {fullWeatherData && (
           <>
-            <ResumeWeather
-              fullWeatherData={fullWeatherData}
-              unitOption={unitOption}
-              getWeatherFunction={getWeatherByName}
-              setError={setError}
-            />
+            <ResumeWeather fullWeatherData={fullWeatherData} unitOption={unitOption} getWeatherFunction={getWeatherByName} />
             <DetailWeather fullWeatherData={fullWeatherData} setUnitOption={setUnitOption} unitOption={unitOption} />
           </>
         )}
